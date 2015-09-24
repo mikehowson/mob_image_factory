aef32e2c8f7df9053e01679d5632e9ff3cbe9aba
Mike Howson <mike.howson@p4innovation.com>
2015-09-24 09:42:05 +0100

    Now convert all android icon sizes

--- lib/mob_image_factory.rb
+++ lib/mob_image_factory.rb
@@ -7,22 +7,40 @@ class MobImageFactory
     if @args.empty?
       return usage
     end
-    filename = @args[0]
+    @infilename = @args[0]
+    @outfilename = @args[1].nil? ? @infilename : @args[1]
 
-    system "identify #{filename}"
+    system "identify #{@infilename}"
 
-    convert filename, 144, "android", "xxhdpi"
+    #Android App icons
+    convert 192, "android", "xxxhdpi"
+    convert 144, "android", "xxhdpi"
+    convert 96, "android", "xhdpi"
+    convert 72, "android", "hdpi"
+    convert 48, "android", "mdpi"
+
+    #Android Notification icons
+    convert_greyscale 96, "android", "xxxhdpi", "notification_"
+    convert_greyscale 72, "android", "xxhdpi", "notification_"
+    convert_greyscale 48, "android", "xhdpi", "notification_"
+    convert_greyscale 36, "android", "hdpi", "notification_"
+    convert_greyscale 24, "android", "mdpi", "notification_"
+
     "Done"
   end
 
 private
-  def convert(filename, size, platform, folder)
+  def convert_greyscale(size, platform, folder, filename_prepend = "")
+    convert size, platform, folder, filename_prepend, "-colorspace Gray"
+  end
+
+  def convert(size, platform, folder, filename_prepend = "", extra_cmd_options = "")
     system "mkdir #{platform}"
     system "mkdir #{platform}/#{folder}"
-    system "convert #{filename} -resize #{size}x#{size} #{platform}/#{folder}/#{filename}"
+    system "convert #{@infilename} #{extra_cmd_options} -resize #{size}x#{size} #{platform}/#{folder}/#{filename_prepend}#{@outfilename}"
   end
 
   def usage
-    "Usage: mob_image_factory <filename>"
+    "Usage: mob_image_factory <filename> <optional_output_filename>"
   end
 end
